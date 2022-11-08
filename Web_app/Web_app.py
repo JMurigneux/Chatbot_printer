0ed7de9dd2404a22ca990fa26f995f780797a7fb
JMurigneux <joannes.muri@gmail.com>
2022-11-08 21:15:33 +0100

    Update Web_app.py

--- Web_app/Web_app.py
+++ Web_app/Web_app.py
@@ -23,7 +23,7 @@ if 'agenda' not in st.session_state:
     st.session_state["agenda"]=[[],[],[],[],[],[]]
 
 if 'model' not in st.session_state:
-    st.session_state["model"]=cl.import_model("Streamlit_rakuten/model.pickle")
+    st.session_state["model"]=cl.import_model("/app/chatbot_printer/Web_app/model.pickle")
 
 jour=["lundi","mardi","mercredi",'jeudi',"dimanche"]
 
